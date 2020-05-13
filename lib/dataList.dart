ca3caff5df1f6a19a5e2a4746470da856d60e0c7
koudai <61653253+koudai0619@users.noreply.github.com>
2020-05-13 23:33:14 +0900

    リストをmapに変更した。次回からは追加、削除などの機能をfirebaseを用いて実装していく必要がある。

--- lib/dataList.dart
+++ lib/dataList.dart
@@ -3,11 +3,18 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 class ListData extends ChangeNotifier {
-  List<String> wantList = [];
-  int get listCount {
-    return wantList.length;
-  }
+//  List<String> wantList = [];
+//  int get listCount {
+//    return wantList.length;
+//  }
 
+  Map<DateTime, List> wantList = {
+    DateTime.now().subtract(Duration(days: 1)): [],
+    DateTime.now(): [],
+    DateTime.now().add(Duration(days: 1)): []
+  };
+
+  //todo firestoreに保存しているデータに追加できるようにする
   void addTask(String newTaskTitle) {
     wantList.add(newTaskTitle);
     notifyListeners(); //完了したら通知を送ってデータの更新を行う必要がある。
